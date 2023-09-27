5c060b8692757152bf52907755e1788f8fe05a6d
Sebastian <seb154@live.com>
2023-09-27 13:12:50 -0400

    modifies index.js and Adds package

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+  (updatedEmployee[key] = value);
+  return updatedEmployee;
+  }
+  const employee = {
+    name: '', 
+    key: '',
+    value: ''
+  };
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, key, value);
+  console.log(updatedEmployee);
+ 
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+   const destructivelyUpdatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value);
+  console.log(destructivelyUpdatedEmployee);
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
